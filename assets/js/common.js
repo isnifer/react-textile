e4fb329a2477a1ae659e333a9b18d745a8678755
Anton Kuznetsov <isnifer@gmail.com>
2014-07-29 19:13:09 +0400

    Calculate overall and current value

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,7 +8,9 @@
 
   var Main = React.createClass({displayName: 'Main',
     getInitialState: function () {
-      return {data: []};
+      return {
+        data: []
+      };
     },
     loadData: function () {
       $.ajax({
@@ -22,16 +24,34 @@
         }
       });
     },
+    sizeCallback: function (size, count, value) {
+      var sizeTitle = size.props.title;
+      var parentTitle = size.props.parentTitle;
+
+      var tempData = this.state.data;
+
+      var currentParrentIndex = tempData.findIndex(function (el, i) {
+        return (el.title == parentTitle);
+      });
+
+      tempData[currentParrentIndex].count += count; 
+      tempData[currentParrentIndex].value += value;
+      
+      this.setState({data: tempData});
+
+      console.log(this.state.data);
+
+    },
     componentDidMount: function () {
-        this.loadData();
+      this.loadData();
     },
     render: function () {
-        return (
-            React.DOM.div({className: "main__wrapper"}, 
-                Sidebar({data: this.state.data}), 
-                Content({data: this.state.data})
-            )
-        );
+      return (
+        React.DOM.div({className: "main__wrapper"}, 
+          Sidebar({data: this.state.data}), 
+          Content({data: this.state.data, sizeCallback: this.sizeCallback})
+        )
+      ); 
     }
   });
 
@@ -41,31 +61,37 @@
 
   var Sidebar = React.createClass({displayName: 'Sidebar',
     render: function () {
-        var items = this.props.data.map(function (item, i) {
-            return SidebarItem({url: '#/product/' + i, title: item.title})
-        });
-        return (
-            React.DOM.div({className: "sidebar"}, 
-                React.DOM.ul({className: "sidebar__list"}, 
-                    items
-                ), 
-                React.DOM.div({className: "sidebar__all"}, "Всего на $", this.props.all), 
-                React.DOM.button({type: "button", className: "sidebar__btn btn btn-success"}, "Отправить заказ менеджеру")
-            )
-        );
+      var overall = this.props.data.reduce(function(prev, current) {
+        return prev + current.value;
+      }, 0);
+      var items = this.props.data.map(function (item, i) {
+        return SidebarItem({url: '#/product/' + i, title: item.title, value: item.value, count: item.count})
+      });
+      return (
+        React.DOM.div({className: "sidebar"}, 
+          React.DOM.div({className: "sidebar__list"}, 
+            items
+          ), 
+          React.DOM.div({className: "sidebar__all"}, "Всего на ", overall, " руб."), 
+          React.DOM.button({type: "button", className: "sidebar__btn btn btn-success"}, "Отправить заказ менеджеру")
+        )
+      );
     }
   });
 
   var SidebarItem = React.createClass({displayName: 'SidebarItem',
-      render: function () {
-          return (
-              React.DOM.li({className: "sidebar__item"}, 
-                  React.DOM.a({className: "sidebar__link", href: this.props.url}, 
-                      this.props.title
-                  )
-              )
-          );
-      }
+    render: function () {
+      return (
+        React.DOM.div({className: "sidebar__item"}, 
+          React.DOM.a({className: "sidebar__link", href: this.props.url}, 
+            this.props.title
+          ), 
+          React.DOM.div({className: "sidebar__choose"}, 
+            "Выбрано ", this.props.count, " комплектов на ", this.props.value, " руб."
+          )
+        )
+      );
+    }
   });
 
   /* SIDEBAR COMPONENTS END */
@@ -90,12 +116,19 @@
     render: function () {
       var _this = this;
       var variants = this.props.variants.map(function (variant) {
-        return Variant({title: variant.title, sizes: variant.sizes, sizeCallback: _this.props.sizeCallback})
+        return Variant({title: variant.title, sizes: variant.sizes, parentTitle: _this.props.title, sizeCallback: _this.props.sizeCallback})
+      });
+      var overallItems = this.props.variants[0].sizes.map(function (size) {
+        return ProductOverallItem({title: size.title})
       });
       return (
         React.DOM.div({className: "product"}, 
-          React.DOM.div({className: "product__stat"}, 
-            React.DOM.div({className: "product__title"}, this.props.title)
+          React.DOM.div({className: "product__stat g-clf"}, 
+            React.DOM.div({className: "product__title"}, this.props.title), 
+            React.DOM.div({className: "product__overall"}, 
+              React.DOM.div({className: "product__overall-title"}, "Общее количество:"), 
+              overallItems
+            )
           ), 
           React.DOM.div({className: "product__variants"}, 
             variants
@@ -105,11 +138,22 @@
     }
   });
 
+  var ProductOverallItem = React.createClass({displayName: 'ProductOverallItem',
+    render: function () {
+      return (
+        React.DOM.div({className: "product__overall-item"}, 
+          React.DOM.div({className: "product__overall-item-title"}, this.props.title), 
+          React.DOM.div({className: "product__overall-item-count"}, "шт")
+        )
+      );
+    }
+  });
+
   var Variant = React.createClass({displayName: 'Variant',
     render: function () {
       var _this = this;
       var sizes = this.props.sizes.map(function (size) {
-        return VariantSize({title: size.title, value: size.value, active: size.active, callback: _this.props.sizeCallback})
+        return VariantSize({title: size.title, price: size.price, active: size.active, parentTitle: _this.props.parentTitle, sizeCallback: _this.props.sizeCallback})
       });
       return (
         React.DOM.div({className: "variant"}, 
@@ -124,28 +168,47 @@
 
   var VariantSize = React.createClass({displayName: 'VariantSize',
     getInitialState: function () {
-      return {count: 0};
+      return {
+        count: 0,
+        value: 0
+      };
     },
     changeValue: function (e) {
-      this.setState({count: e.target.value});
+      this.setState({
+        count: e.target.value,
+        value: e.target.value * this.props.price
+      });
+      this.props.sizeCallback(this, e.target.value, e.target.value * this.props.price);
     },
     plusValue: function () {
-      this.setState({count: this.state.count += 1});
+      var count = this.state.count + 1;
+      var value = count * this.props.price;
+      this.setState({
+        count: count,
+        value: value
+      });
+      this.props.sizeCallback(this, 1, this.props.price);
     },
     minusValue: function () {
-      this.setState({count: (this.state.count === 0) ? 0 : this.state.count -= 1});
+      var count = (this.state.count === 0) ? 0 : this.state.count - 1;
+      var value = count * this.props.price; 
+      this.setState({
+        count: count,
+        value: value
+      });
+      this.props.sizeCallback(this, (this.state.count === 0) ? 0: -1, (this.state.count === 0) ? 0 : -this.props.price);
     },
     render: function () {
       return (
         React.DOM.div({className: (this.props.active) ? 'size' : 'size size_state_inactive'}, 
           React.DOM.p({className: "size__title"}, this.props.title), 
-          React.DOM.p({className: "size__price"}, this.props.value, ".-"), 
+          React.DOM.p({className: "size__price"}, this.props.price, ".-"), 
           React.DOM.p({className: "size__counter g-clf"}, 
             React.DOM.i({className: "size__changer glyphicon glyphicon-minus", onClick: this.minusValue}), 
-            React.DOM.input({type: "text", className: "size__input", maxlength: "4", value: this.state.count, disabled: !this.props.active, onChange: this.changeValue}), 
+            React.DOM.input({type: "text", className: "size__input", maxLength: "4", value: this.state.count, disabled: !this.props.active, onChange: this.changeValue}), 
             React.DOM.i({className: "size__changer glyphicon glyphicon-plus", onClick: this.plusValue})
           ), 
-          React.DOM.p({className: "size__value"}, this.state.count * this.props.value, ".-")
+          React.DOM.p({className: "size__value"}, this.state.value, ".-")
         )
       );
     }
--- src/js/common.js
+++ src/js/common.js
@@ -8,7 +8,9 @@
 
   var Main = React.createClass({
     getInitialState: function () {
-      return {data: []};
+      return {
+        data: []
+      };
     },
     loadData: function () {
       $.ajax({
@@ -22,16 +24,34 @@
         }
       });
     },
+    sizeCallback: function (size, count, value) {
+      var sizeTitle = size.props.title;
+      var parentTitle = size.props.parentTitle;
+
+      var tempData = this.state.data;
+
+      var currentParrentIndex = tempData.findIndex(function (el, i) {
+        return (el.title == parentTitle);
+      });
+
+      tempData[currentParrentIndex].count += count; 
+      tempData[currentParrentIndex].value += value;
+      
+      this.setState({data: tempData});
+
+      console.log(this.state.data);
+
+    },
     componentDidMount: function () {
-        this.loadData();
+      this.loadData();
     },
     render: function () {
-        return (
-            <div className="main__wrapper">
-                <Sidebar data={this.state.data} />
-                <Content data={this.state.data} />
-            </div>
-        );
+      return (
+        <div className="main__wrapper">
+          <Sidebar data={this.state.data} />
+          <Content data={this.state.data} sizeCallback={this.sizeCallback} />
+        </div>
+      ); 
     }
   });
 
@@ -41,31 +61,37 @@
 
   var Sidebar = React.createClass({
     render: function () {
-        var items = this.props.data.map(function (item, i) {
-            return <SidebarItem url={'#/product/' + i} title={item.title} />
-        });
-        return (
-            <div className="sidebar">
-                <ul className="sidebar__list">
-                    {items}
-                </ul>
-                <div className="sidebar__all">Всего на ${this.props.all}</div>
-                <button type="button" className="sidebar__btn btn btn-success">Отправить заказ менеджеру</button>
-            </div>
-        );
+      var overall = this.props.data.reduce(function(prev, current) {
+        return prev + current.value;
+      }, 0);
+      var items = this.props.data.map(function (item, i) {
+        return <SidebarItem url={'#/product/' + i} title={item.title} value={item.value} count={item.count} />
+      });
+      return (
+        <div className="sidebar">
+          <div className="sidebar__list">
+            {items}
+          </div>
+          <div className="sidebar__all">Всего на {overall} руб.</div>
+          <button type="button" className="sidebar__btn btn btn-success">Отправить заказ менеджеру</button>
+        </div>
+      );
     }
   });
 
   var SidebarItem = React.createClass({
-      render: function () {
-          return (
-              <li className="sidebar__item">
-                  <a className="sidebar__link" href={this.props.url}>
-                      {this.props.title}
-                  </a>
-              </li>
-          );
-      }
+    render: function () {
+      return (
+        <div className="sidebar__item">
+          <a className="sidebar__link" href={this.props.url}>
+            {this.props.title}
+          </a>
+          <div className="sidebar__choose">
+            Выбрано {this.props.count} комплектов на {this.props.value} руб.
+          </div>
+        </div>
+      );
+    }
   });
 
   /* SIDEBAR COMPONENTS END */
@@ -90,12 +116,19 @@
     render: function () {
       var _this = this;
       var variants = this.props.variants.map(function (variant) {
-        return <Variant title={variant.title} sizes={variant.sizes} sizeCallback={_this.props.sizeCallback} />
+        return <Variant title={variant.title} sizes={variant.sizes} parentTitle={_this.props.title} sizeCallback={_this.props.sizeCallback} />
+      });
+      var overallItems = this.props.variants[0].sizes.map(function (size) {
+        return <ProductOverallItem title={size.title} />
       });
       return (
         <div className="product">
-          <div className="product__stat">
+          <div className="product__stat g-clf">
             <div className="product__title">{this.props.title}</div>
+            <div className="product__overall">
+              <div className="product__overall-title">Общее количество:</div>
+              {overallItems}
+            </div>
           </div>
           <div className="product__variants">
             {variants}
@@ -105,11 +138,22 @@
     }
   });
 
+  var ProductOverallItem = React.createClass({
+    render: function () {
+      return (
+        <div className="product__overall-item">
+          <div className="product__overall-item-title">{this.props.title}</div>
+          <div className="product__overall-item-count">шт</div>
+        </div>
+      );
+    }
+  });
+
   var Variant = React.createClass({
     render: function () {
       var _this = this;
       var sizes = this.props.sizes.map(function (size) {
-        return <VariantSize title={size.title} value={size.value} active={size.active} callback={_this.props.sizeCallback} />
+        return <VariantSize title={size.title} price={size.price} active={size.active} parentTitle={_this.props.parentTitle} sizeCallback={_this.props.sizeCallback} />
       });
       return (
         <div className="variant">
@@ -124,28 +168,47 @@
 
   var VariantSize = React.createClass({
     getInitialState: function () {
-      return {count: 0};
+      return {
+        count: 0,
+        value: 0
+      };
     },
     changeValue: function (e) {
-      this.setState({count: e.target.value});
+      this.setState({
+        count: e.target.value,
+        value: e.target.value * this.props.price
+      });
+      this.props.sizeCallback(this, e.target.value, e.target.value * this.props.price);
     },
     plusValue: function () {
-      this.setState({count: this.state.count += 1});
+      var count = this.state.count + 1;
+      var value = count * this.props.price;
+      this.setState({
+        count: count,
+        value: value
+      });
+      this.props.sizeCallback(this, 1, this.props.price);
     },
     minusValue: function () {
-      this.setState({count: (this.state.count === 0) ? 0 : this.state.count -= 1});
+      var count = (this.state.count === 0) ? 0 : this.state.count - 1;
+      var value = count * this.props.price; 
+      this.setState({
+        count: count,
+        value: value
+      });
+      this.props.sizeCallback(this, (this.state.count === 0) ? 0: -1, (this.state.count === 0) ? 0 : -this.props.price);
     },
     render: function () {
       return (
         <div className={(this.props.active) ? 'size' : 'size size_state_inactive'}>
           <p className="size__title">{this.props.title}</p>
-          <p className="size__price">{this.props.value}.-</p>
+          <p className="size__price">{this.props.price}.-</p>
           <p className="size__counter g-clf">
             <i className="size__changer glyphicon glyphicon-minus" onClick={this.minusValue} />
-            <input type="text" className="size__input" maxlength="4" value={this.state.count} disabled={!this.props.active} onChange={this.changeValue} /> 
+            <input type="text" className="size__input" maxLength="4" value={this.state.count} disabled={!this.props.active} onChange={this.changeValue} /> 
             <i className="size__changer glyphicon glyphicon-plus" onClick={this.plusValue} />
           </p>
-          <p className="size__value">{this.state.count * this.props.value}.-</p>
+          <p className="size__value">{this.state.value}.-</p>
         </div>
       );
     }
